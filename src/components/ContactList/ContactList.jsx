39a684304ed6c6302cecb2887c6b22a8d3d36a60
ViktoriiaKN <viktoriya_nykyforova@i.ua>
2024-08-17 21:39:59 +0200

    Search fixed

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,21 @@
 import { useSelector } from 'react-redux';
 import { selectContacts } from '../../redux/contactsSlice';
+import { selectNameFilter } from '../../redux/filtersSlice'; 
 import style from './ContactList.module.css';
 import Contact from '../Contact/Contact';
 
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter); 
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  ); 
 
   return (
     <div>
       <ul className={style.contactList}>
-        {contacts.map(contact => (
+        {filteredContacts.map(contact => (
           <Contact key={contact.id} contact={contact} />
         ))}
       </ul>
